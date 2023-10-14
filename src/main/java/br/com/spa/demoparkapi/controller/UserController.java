1582c1779514cea27747478c47c84b0af667a29a
Paulo César Passos Pereira <paulo.passos1@gmail.com>
2023-10-14 09:18:05 -0300

    criação do recurso de adicionar user

--- src/main/java/br/com/spa/demoparkapi/controller/UserController.java
+++ src/main/java/br/com/spa/demoparkapi/controller/UserController.java
@@ -1,14 +1,26 @@
 package br.com.spa.demoparkapi.controller;
 
+import br.com.spa.demoparkapi.entity.User;
 import br.com.spa.demoparkapi.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequiredArgsConstructor
 @RestController
 @RequestMapping("api/v1/users")
 public class UserController {
 
-    private final UserService userService;
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public ResponseEntity<User> addUser(@RequestBody User user){
+       user = this.userService.create(user);
+       return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
 }
--- src/main/java/br/com/spa/demoparkapi/service/UserService.java
+++ src/main/java/br/com/spa/demoparkapi/service/UserService.java
@@ -1,14 +1,20 @@
 package br.com.spa.demoparkapi.service;
 
+import br.com.spa.demoparkapi.entity.User;
 import br.com.spa.demoparkapi.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-@RequiredArgsConstructor
 @Service
 public class UserService {
 
+    @Autowired
     private UserRepository userRepository;
 
-
+    @Transactional
+    public User create(User user){
+        return this.userRepository.save(user);
+    }
 }
