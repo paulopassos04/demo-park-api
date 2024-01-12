b8f7b1d19f90ff85e1162591af3acedc18c6045b
Paulo César Passos Pereira <paulo.passos1@gmail.com>
2024-01-12 15:19:47 -0300

    implementando a auditoria do usuário

--- src/main/java/br/com/spa/demoparkapi/entity/User.java
+++ src/main/java/br/com/spa/demoparkapi/entity/User.java
@@ -3,6 +3,11 @@ package br.com.spa.demoparkapi.entity;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.Objects;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -12,21 +17,33 @@ import lombok.*;
 @NoArgsConstructor
 @Entity
 @Table(name = "tb_users")
-public class User implements Serializable{
-    
+@EntityListeners(AuditingEntityListener.class)
+public class User implements Serializable {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(nullable = false, unique = true, length = 100)
     private String username;
+
     @Column(nullable = false, unique = false, length = 200)
     private String password;
+
     @Enumerated(EnumType.STRING)
     @Column(nullable = false, length = 25)
     private Role role = Role.ROLE_CLIENT;
+
+    @CreatedDate
     private LocalDateTime date_create;
+
+    @LastModifiedDate
     private LocalDateTime date_update;
+
+    @CreatedBy
     private String created_by;
+
+    @LastModifiedBy
     private String modified_by;
 
     public enum Role {
@@ -35,8 +52,10 @@ public class User implements Serializable{
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
         User user = (User) o;
         return Objects.equals(id, user.id);
     }
--- src/main/java/br/com/spa/demoparkapi/service/UserService.java
+++ src/main/java/br/com/spa/demoparkapi/service/UserService.java
@@ -23,54 +23,55 @@ public class UserService {
     private PasswordEncoder passwordEncoder;
 
     @Transactional
-    public User create(User user){
+    public User create(User user) {
         try {
             user.setPassword(passwordEncoder.encode(user.getPassword()));
             return this.userRepository.save(user);
-        } catch (DataIntegrityViolationException ex){
-            throw new UserNameUniqueViolationException(String.format("Username '%s' já cadastrado", user.getUsername()));
+        } catch (DataIntegrityViolationException ex) {
+            throw new UserNameUniqueViolationException(
+                    String.format("Username '%s' já cadastrado", user.getUsername()));
         }
 
     }
 
     @Transactional(readOnly = true)
-    public User findByUsername(String username){
+    public User findByUsername(String username) {
         Optional<User> user = Optional.ofNullable(this.userRepository.findByUsername(username));
 
-        return user.orElseThrow(() -> new EntityNotFoundException(String.format("Usuário username '%s' não encontrado", username)));
+        return user.orElseThrow(
+                () -> new EntityNotFoundException(String.format("Usuário username '%s' não encontrado", username)));
     }
 
-
     @Transactional(readOnly = true)
-    public User.Role findRoleByUsername(String username){
+    public User.Role findRoleByUsername(String username) {
         return userRepository.findRoleByUsername(username);
     }
 
-    public User findById(Long id){
+    public User findById(Long id) {
         return userRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Usuario não encontrado"));
     }
 
     @Transactional(readOnly = true)
-    public List<User> findAll(){
+    public List<User> findAll() {
         return this.userRepository.findAll();
     }
 
     @Transactional
-    public User updatePassword(Long id, String currentPassword, String newPassword, String confirmPassword){
-        if (!newPassword.equals(confirmPassword)){
+    public User updatePassword(Long id, String currentPassword, String newPassword, String confirmPassword) {
+        if (!newPassword.equals(confirmPassword)) {
             throw new PasswordInvalidException(String.format("Nova senha não confere com confirmação de senha."));
         }
 
         User user = findById(id);
-        if (!passwordEncoder.matches(confirmPassword, user.getPassword())){
+        if (!passwordEncoder.matches(currentPassword, user.getPassword())) {
             throw new PasswordInvalidException(String.format("Sua senha não confere"));
         }
-       user.setPassword(passwordEncoder.encode(newPassword));
+        user.setPassword(passwordEncoder.encode(newPassword));
         return user;
     }
 
     @Transactional
-    public void delete(Long id){
+    public void delete(Long id) {
         findById(id);
         userRepository.deleteById(id);
     }
