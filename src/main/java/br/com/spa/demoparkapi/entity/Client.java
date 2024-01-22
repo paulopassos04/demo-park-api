8432ca5f9c10039ce574d559026aafd19f9d5205
Paulo César Passos Pereira <paulo.passos1@gmail.com>
2024-01-22 09:17:21 -0300

    criação do modelo cliente

---
+++ src/main/java/br/com/spa/demoparkapi/entity/Client.java
@@ -0,0 +1,65 @@
+package br.com.spa.demoparkapi.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_clients")
+@EntityListeners(AuditingEntityListener.class)
+public class Client implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = false, length = 11, unique = true)
+    private String cpf;
+
+    @OneToOne
+    @JoinColumn(name = "id_user", nullable = false)
+    private User user;
+
+    @CreatedDate
+    private LocalDateTime date_create;
+
+    @LastModifiedDate
+    private LocalDateTime date_update;
+
+    @CreatedBy
+    private String created_by;
+
+    @LastModifiedBy
+    private String modified_by;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Client client = (Client) o;
+        return Objects.equals(id, client.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+}
